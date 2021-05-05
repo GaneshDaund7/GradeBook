5c94b02fb23786e4f57af3ad14253d138b4cacae
Ganesh Daund <57858907+GaneshDaund7@users.noreply.github.com>
2021-05-05 15:53:45 +0530

    First Commit

---
+++ GradeBook/Books.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GradeBook
+{
+    class Books
+    {
+        List<double> Grades=new List<double>();
+         string name;
+         public Books(String name)
+        {
+            this.name = name;
+            Console.WriteLine("Welome to " + name);
+        }
+        public void AddGrades(double grade)
+        {
+            Grades.Add(grade);
+        }
+
+        public void ShowStats()
+        {
+            var res = 0.0;
+            var LowGrade = Grades[0];
+            var HighGrade = Grades[0];
+            foreach (var i in Grades)
+            {
+                if (HighGrade < i)
+                    HighGrade = i;
+                if (LowGrade> i)
+                    LowGrade = i;
+                res += i;
+            }
+                
+            res /= Grades.Count;
+            Console.WriteLine("The HighestGarde is " + HighGrade);
+            Console.WriteLine("The LowestGrade is " + LowGrade);
+            Console.WriteLine("The Average Garde is "+ res);
+            Console.WriteLine();
+           
+        }
+        
+    }
+}
---
+++ GradeBook/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book1 = new Books("GD's Grade Book");
+            book1.AddGrades(9.9);
+            book1.AddGrades(7.8);
+            book1.AddGrades(7.9);
+            book1.AddGrades(8.5);
+            book1.AddGrades(8.6);
+            book1.ShowStats();
+
+
+
+            var book2 = new Books("GK's Grade Book");
+            book2.AddGrades(9.7);
+            book2.AddGrades(8.7);
+            book2.AddGrades(7.7);
+            book2.AddGrades(9.5);
+            book2.AddGrades(7.9);
+            book2.ShowStats();
+
+            var book3 = new Books("SS's Grade Book");
+            book3.AddGrades(9.99);
+            book3.AddGrades(8.9);
+            book3.AddGrades(9.6);
+            book3.AddGrades(8.7);
+            book3.AddGrades(8.9);
+            book3.ShowStats();
+
+
+            Console.ReadLine();
+        }
+    }
+}
